61aa8a4ae1b5a8ee39ec848587a5a628a2acd063
simona_g <sglombikova@gmail.com>
2019-11-17 17:43:24 +0100

    Added model

---
+++ src/models/model.py
@@ -0,0 +1,43 @@
+import tensorflow.keras as keras
+
+
+class SoundAnimalDetector(keras.Model):
+    def __init__(self):
+        super(SoundAnimalDetector, self).__init__()
+        self.model_layers = {
+            keras.layers.Conv2D(
+                filters=16,
+                kernel_size=3,
+                padding='same',
+                activation='relu'),
+            keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+            keras.layers.Conv2D(
+                filters=32,
+                kernel_size=3,
+                padding='same',
+                activation='relu'),
+            keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+            keras.layers.Conv2D(
+                filters=64,
+                kernel_size=3,
+                padding='same',
+                activation='relu'),
+            keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+            keras.layers.Flatten(),
+
+            keras.layers.Dense(
+                units=512,
+                activation='relu'),
+
+            keras.layers.Dense(
+                units=dim_output,
+                activation='softmax')
+        }
+
+    def call(self, x):
+        for layer in self.model_layers:
+            x = layer(x)
+            return x
