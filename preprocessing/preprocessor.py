7a60e3dc9885c525ca35fb2b2e325a041e8e4f31
simona_g <sglombikova@gmail.com>
2019-11-17 17:41:06 +0100

    Added preprocessing from mp3 to wav to mffc spectogram

--- preprocessing/preprocessor.py
+++ preprocessing/preprocessor.py
@@ -10,13 +10,41 @@ def detect_leading_silence(sound, silence_threshold=-50.0, chunk_size=10):
     :param chunk_size: in ms
     :return: start time of first sound
     """
-    trim_ms = 0 # ms
+    trim_ms = 0  # ms
 
-    assert chunk_size > 0 # to avoid infinite loop
-    while sound[trim_ms:trim_ms+chunk_size].dBFS < silence_threshold and trim_ms < len(sound):
+    assert chunk_size > 0  # to avoid infinite loop
+    while sound[trim_ms:trim_ms + chunk_size].dBFS < silence_threshold and trim_ms < len(sound):
         trim_ms += chunk_size
 
     return trim_ms
+
+def from_mp3_to_wav(audio_path):
+    """
+       Implement convertor from mp3 to wav
+
+       :param audio_path: path to audio
+       """
+    sound = AudioSegment.from_mp3(audio_path)
+    sound.export(audio_path.replace('.mp3', '.wav'), format="wav")
+
+def mfcc_spectogram(file_name):
+    """
+           Implement extract features from wav file to mfcc spectogram features
+
+           :param audio_path: path to audio
+           :return: Mfcc spectogram in numerical array
+    """
+    try:
+        audio, sample_rate = librosa.load(file_name, res_type='kaiser_fast')
+        mfccs = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
+        mfccsscaled = numpy.mean(mfccs.T, axis=0)
+
+    except Exception as e:
+        print("Error encountered while parsing file: ", file_name)
+        return None
+
+    return mfccsscaled
+
 
 # TODO doimplementovať
 sound = AudioSegment.from_file("/path/to/file.wav", format="wav")
@@ -25,4 +53,4 @@ start_trim = detect_leading_silence(sound)
 end_trim = detect_leading_silence(sound.reverse())
 
 duration = len(sound)
-trimmed_sound = sound[start_trim:duration-end_trim]
+trimmed_sound = sound[start_trim:duration - end_trim]
