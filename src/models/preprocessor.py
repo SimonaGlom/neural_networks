74a96c081301c7b61a58fb731369f63fa631230b
simona_g <sglombikova@gmail.com>
2019-12-01 14:15:50 +0100

    Changed way of scaling mfccs

--- src/models/preprocessor.py
+++ src/models/preprocessor.py
@@ -63,8 +63,6 @@ def mfcc_spectogram(file_name):
     try:
         audio, sample_rate = librosa.load(file_name, res_type='kaiser_fast')
         mfccs = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
-        mfccsscaled = numpy.mean(mfccs.T, axis=0)
-        print(sample_rate, mfccs)
 
     except Exception as e:
         print("Error encountered while parsing file: ", file_name)
